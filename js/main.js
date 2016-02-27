6464f3f274e8947a5f1cacf906e5be346ea6acd3
acatella <aficclark@gmail.com>
2016-02-27 15:59:24 -0800

    added dropdowns started writing js algorithm

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//Custom JS for Reddit Top 5 Web App
+
+//ready document
+
+
+//When a subreddit is chosen, populate video files w/ top 5 gifs on
+//for chosen date range
+
+//default date range is Today
+
+//function to build url
+//set values of subreddit and date range
+//add those into URL to and return full URL
+  //filter using date range
+  //sort by home page
+
+//function to return gifs
+//declare variable and point to result of buildURL function
+//make json call using url variable
