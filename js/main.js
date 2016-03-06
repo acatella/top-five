3a8206aaf7482c051f6a17d66c4d393008219b76
acatella <aficclark@gmail.com>
2016-03-05 16:42:47 -0800

    wrote code to populate videos and change hero video on click

--- js/main.js
+++ js/main.js
@@ -5,6 +5,13 @@ $(document).ready(function() {
 //When a subreddit is chosen, populate video files w/ top 5 gifs on
 //for chosen date range
 
+//TODO: updated hero vid when thumb is clicked
+//TODO: add hover effect that shows post title and number of upvotes
+//TODO: limit poster size to thumbnail size
+//TODO: what to do if gif or poster aren't valid links anymore?
+//TODO: incorporate gifs from other sources
+//TODO: add player controls
+
 //Controls subreddit and range selector dropdowns
 var currentSub = "soccer";
 var currentRange = "today";
@@ -17,9 +24,24 @@ $("[aria-labelledby='subreddit-dropdown'] a").on('click', function(e) {
 
 $("[aria-labelledby='date-dropdown'] a").on('click', function(e) {
   var clicked = $(e.target);
-  currentRange = clicked.attr('id');
-  $('#date-dropdown').html(currentRange+"  <span class='caret'></span>");
+  currentRange = clicked.attr('data-value');
+  $('#date-dropdown').html(clicked.attr('id') +"  <span class='caret'></span>");
   gifGen();
+});
+
+//Updates hero video when a thumb is clicked
+$('.thumb').on('click', function(e) {
+  var $clickedGif = $(e.target);
+  var $heroCont = $('#hero-container');
+  var $heroGif = $heroCont.find('.gif');
+  $heroGif.addClass('thumb');
+  $heroGif.removeAttr('controls');
+  $heroGif.removeAttr('autoplya');
+  $clickedGif.removeClass('thumb');
+  $clickedGif.attr('controls','');  
+  $clickedGif.parent().empty().append($heroGif);
+  $heroCont.empty().append($clickedGif);
+
 });
 
 //function to build url
@@ -31,41 +53,61 @@ function buildURL(sub,range) {
 //function to return gifs
 function gifGen() {
   $.getJSON(buildURL(currentSub,currentRange)).done(function(listing) {
-    console.log(buildURL(currentSub, currentRange));
+
+    //testing line
+    // console.log(buildURL(currentSub, currentRange));
+
     var posts = listing.data.children; //array of all posts
-    var limit = 0; //counter variable
+    var counter = 0; //counter variable
+    var $heroDiv = $('#hero-container');
+
     $.each(posts, function(index,post) {
       if (!post.data.domain.search("streamable")) {
-        limit++
+        counter++
         var postURL = post.data.url;
         var urlArray = postURL.split("/"); //break URL into array
         var uniqueID = urlArray[urlArray.length-1];
 
         var thumbSrc = "https://cdn.streamable.com/image/" + uniqueID + ".jpg";
         var gifSrc = "https://cdn.streamable.com/video/mp4/" + uniqueID + ".mp4";
-        console.log(gifSrc);
-        //TODO: everything is working but the objects i get from the fall are different than what i see in the json
-      }
+
+        //testing line
+        // console.log(gifSrc);
+
+        var $gifEl = $('<video>');
+
+        $gifEl.attr({
+          'loop': '',
+          'preload': 'none',
+          // 'autoplay': '',
+          'poster': thumbSrc,
+          'id': 'gif' + counter,
+          'class': 'gif',
+        });
+
+
+        var $srcEl = $('<source>').attr({
+          'src': gifSrc,
+          'type': 'video/mp4',
+        });
+
+        $gifEl.append($srcEl);
+
+        if (counter == 1) {
+          $gifEl.attr({
+            'controls': '',
+            'autoplay': ''
+          });
+          $heroDiv.empty();
+          $heroDiv.append($gifEl);
+        }
+
+        else {
+          var $gifDiv = $('#gifDiv' + counter);
+          $gifDiv.empty().append($gifEl);
+        }
+      };
     });
-    //if listing.data.children[n].data.domain is 'streamable'
-      //+1 to counter variable
-      //extract video ID from end of url - whatever comes after '.com/'
-        //var uniqueID = post.data.url.split("/")[post.data.url.length-1]
-      //build poster url 'https://cdn.streamable.com/image/' + uniqueID + '.jpg'
-      //build video url 'https://cdn.streamable.com/mp4/' + uniqueID + '.mp4
-      //build html tags
-        //each new tag should have id of #gif[counter-variable]
-        //if the counter is one, it's the hero video
-          //empty #hero-container
-          //append new htmls
-        //else, append the html to #video-thumbs
-        //build info to be displayed when hovered
-          //use title and net score
-          //if title is longer than x chars, shorten it
-
-        //break once the counter variable reaches 5
-
-
   });
 };
 
